3618605ee7e56c04eae7a71a4fb16edae0b0327e
Daniel Wang <danielwang405@gmail.com>
2017-04-29 15:26:44 -0700

    removed content, popup to just have background when icon is clicked

--- background.js
+++ background.js
@@ -1,3 +1,41 @@
 chrome.runtime.onMessage.addListener(function(request, sender) {
-    chrome.tabs.update(sender.tab.id, {url: request.redirect});
+	console.log(request.redirect);
+    //chrome.tabs.update(sender.tab.id, {url: request.redirect});
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.query({
+		active:true,
+		currentWindow: true
+	}, function(tabs) {
+		var tab = tabs[0];
+		var url = tab.url;
+		console.log(url);
+		console.log(tab.id);
+			
+		var xhttp = new XMLHttpRequest();
+		var archiveUrl = "http://archive.org/wayback/available?url="+url
+		xhttp.onreadystatechange = function(){
+			
+			if(xhttp.readyState == 4 && xhttp.status == 200){
+				data = JSON.parse(xhttp.response);
+				var redirectUrl = data.archived_snapshots.closest.url;
+				console.log(redirectUrl)
+				chrome.browserAction.setIcon({path:"iconpressed.png"});
+				chrome.tabs.update(tab.id, {url:redirectUrl});
+
+			}	
+			
+		}
+		
+		
+		xhttp.open("GET",archiveUrl,true)
+		xhttp.send();
+		
+		
+	});
+	
+
+		
+	//chrome.tabs.update(tab.id,{url: "www.google.com"});
 });
